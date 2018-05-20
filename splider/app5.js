f287191f3dc4f5884785bf764d3534fe0511addb
DESKTOP-IK4MUI3\nj <87179471@qq.com>
2018-05-20 16:25:37 +0800

    图片下载写入完成才开始下一个任务

--- splider/app5.js
+++ splider/app5.js
@@ -10,9 +10,9 @@ var fs = require("fs")
 //初始配置
 const config = {
   //起始页(含)
-  startPage: 121,
+  startPage: 201,
   //终止页(含)
-  endPage: 500,
+  endPage: 300,
   //页面编码格式
   ecode: 'gb2312',
   //host
@@ -24,9 +24,9 @@ const config = {
   //请求详情页
   detailsUrls: [],
   //请求并发数
-  queueSum: 1,
+  queueSum: 2,
   //请求间隔ms
-  timeOut: 100,
+  timeOut: 20,
   //重试url
   retry: [],
   //重试失败url
@@ -35,13 +35,21 @@ const config = {
   header: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36' }
 }
 
+//创建目录
+fs.mkdir(config.dir,function(err){  
+  if (err) {
+    console.log('目录已存在')
+  } else {
+    console.log('创建目录成功')
+  }
+})
+
+//所有列表页
 if (config.startPage && config.endPage) {
   for(let i = config.startPage;i <= config.endPage;i ++) {
     config.urls.push('http://www.meizitu.com/a/' + i + '.html')
   }
 }
-
-
 
 console.log('起始页:' + config.startPage)
 console.log('终止页:' + config.endPage)
@@ -73,8 +81,10 @@ function subQueue() {
       console.log(err)
     }
     console.log('副队列完成')
-    console.log(config.retry.length + '条需要重试')
-    oneQueue()
+    if(config.retry.length > 0) {
+      console.log(config.retry.length + '条需要重试')
+      oneQueue()
+    }
   })
 }
 
@@ -90,35 +100,6 @@ function oneQueue() {
     console.log(config.retryErr.length + '条重试出错')
   })
 }
-
-// async.mapLimit(config.urls, queueSum, function (url, callback) {
-//   getDetailsUrls(url, callback)
-// }, function (err, result) {
-//   if(err) {
-//       console.log(err)
-//     }
-//   console.log(config.detailsUrls.length + '个项获取完成')
-
-//   async.mapLimit(config.detailsUrls, queueSum, function (url, callback) {
-
-//     request({
-//       url: url,
-//       timeout: 10000,
-//       headers: config.header
-//     }).on('error', function () {
-//     }).pipe(fs.createWriteStream(config.dir + FileName(url)))
-
-//     setTimeout(function() {
-//       callback(null)
-//     }, config.timeOut)
-
-//   }, function (err, result) {
-//     if(err) {
-//       console.log(err)
-//     }
-//   })
-// })
-
 
 //全局请求配置
 function getPage (url, callback) {
@@ -163,11 +144,14 @@ function getDetails(url, callback, retry) {
     } else {
       config.retry.push(url)
     }
-  }).pipe(fs.createWriteStream(config.dir + FileName(url)))
-
-  setTimeout(function() {
-    callback(null)
-  }, config.timeOut)
+    setTimeout(function() {
+      callback(null)
+    }, config.timeOut)
+  }).pipe(fs.createWriteStream(config.dir + FileName(url)).on('finish', function () {
+    setTimeout(function() {
+      callback(null)
+    }, config.timeOut)
+  }))
 }
 
 //格式化图片名称
