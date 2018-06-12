1fd8445df4cfd3aa3ea1b3248093cd6f97708331
NiJun <87179471@qq.com>
2018-06-12 11:35:22 +0800

    更新小程序demo,toast,modal,获取位置,选择图片,分享页面

--- tchh/pages/test/test.js
+++ tchh/pages/test/test.js
@@ -5,7 +5,12 @@ Page ({
       { desc: '标题一', url: "这是内容！这是内容1" },
       { desc: '标题二', url: "这是内容2！这是内容1" },
       { desc: '标题三', url: "这是内容3！这是内容1" },
-    ]
+    ],
+    imgs: [],
+    location:{
+      longitude: 104.415296,
+      latitude: 31.136743,
+    }
   },
   onLoad: function (page) {
     console.log(page)
@@ -30,5 +35,73 @@ Page ({
         console.log(err)
       }
     })
+  },
+  chooseimg: function () {
+    console.log(this, '选择图片')
+    var _self = this
+    wx.chooseImage({
+      count: 2,
+      success: function (res) {
+        console.log(res.tempFilePaths)
+        _self.setData({
+          imgs: res.tempFilePaths
+        })
+      }
+    })
+  },
+  getlocation: function () {
+    var _self = this
+    wx.getLocation({
+      type: 'gcj02',
+      success: function (res) {
+        console.log(res)
+        _self.setData({
+          location: {
+            latitude: res.latitude,
+            longitude: res.longitude
+          }
+        })
+      },
+      fail: function () {
+        console.log('获取地理位置失败')
+      }
+    })
+  },
+  opentoast: function () {
+    wx.showToast({
+      title: 'toast成功提示',
+      icon: 'success',
+    })
+  },
+  openloading: function () {
+    wx.showLoading({
+      title: '正在加载',
+      success: function () {
+        setTimeout(function () {
+          wx.hideLoading()
+        }, 2000)
+      }
+    })
+  },
+  openmodal: function () {
+    wx.showModal({
+      title: '模态框标题',
+      content: '模态框内容,模态框内容',
+      success: function (res) {
+        console.log(res)
+        if (res.confirm) {
+          wx.showToast({
+            title: '您点击了确定',
+            icon: 'success'
+          })
+        }
+      }
+    })
+  },
+  onShareAppMessage: function (res) {
+    return{
+      title: '转发标题',
+      path: '/pages/test'
+    }
   }
 })
