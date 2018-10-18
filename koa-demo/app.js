37977402e44f34e5dd2c5acd37c22cf56ecfcf9d
NiJun <87179471@qq.com>
2018-10-18 19:45:18 +0800

    跟新 koa_demo

--- koa-demo/app.js
+++ koa-demo/app.js
@@ -1,14 +1,30 @@
 const Koa = require('koa')
 const fs = require('fs')
+const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
 
 const app = new Koa()
+const router = new Router()
 
-const index = (ctx) => {
+app.use(bodyParser())
+
+router.get('/', async (ctx, next) => {
+  let query = ctx.request.query
+  console.log(query)
   let resData =  fs.readFileSync('./view/index.html','utf-8')
   ctx.response.body = resData
-}
+})
 
-app.use(index)
+router.post('/login', async (ctx, next) => {
+  let { name, password } = ctx.request.body
+
+  console.log(name, password)
+
+  ctx.response.body = '密码错误'
+})
+
+
+app.use(router.routes())
 
 app.listen(7000)
 
