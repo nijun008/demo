23ca2a1615337aaa9c849e7d92db2320ab5fc5aa
NJ <87179471@qq.com>
2020-02-26 20:22:12 +0800

    脚本0.1.2

--- undergroundScript.js
+++ undergroundScript.js
@@ -6,120 +6,138 @@ requestScreenCapture(false)
 
 sleep(1000)
 
-var nowLocation = [5, 53] // 当前位置
-var food = 1300 // 剩余食物
-var direction = 'top' // 行走方向
-
-var rigtStep = 0
+var initialLocation = [5, 52] // 初始位置
+var initialFood = 1100 // 初始食物数量
+var initialDirection = 'top'  // 初始行走方向
 
 var mapTop = 14
 var mapRight = 40
 var mapBottom = 40
-var mapLeft = 12
 
+var nowLocation, food, direction, rigtStep
+
+resetData()
+
+// 开始
 nextStep()
 
-function judgeState() {
-  var targetImg = null // 目标图片
-  var targetPath = '../autoImgs/dixiachengbao/target/' // 目标图片路径
-  var i = 1
-  var result = null
 
-  var nowView = captureScreen() // 当前截图
-
-  while(i <= 5 && !result) {
-    i++
-    targetImg = images.read(targetPath + i + '.jpg')
-    result = findImage(nowView, targetImg)
-  }
-  return {
-    state: i,
-    result: result
-  }
-}
-
-// 下一步动作
 function nextStep() {
   var judgeResult = judgeState()
+
   if (!judgeResult.result) {
-    sleep(300)
+    sleep(1000)
     nextStep()
     return
   }
   var x = judgeResult.result.x
   var y = judgeResult.result.y
-  var stopFlag = false
 
   toast(judgeResult.state)
-  // toast(x + ',' + y)
 
   switch (judgeResult.state) {
     case 2: // 交战状态
       // 等待交战完毕
-      sleep(3000)
+      sleep(5000)
       break
     case 3: // 拾取状态
       // 拾取
       click(x, y)
-      sleep(500)
+      sleep(1500)
       break
     case 4: // 采集状态
       // 采集
       click(x, y)
-      sleep(1000)
+      sleep(2000)
       break
     case 5: // 遭遇状态(是否战斗)
       // 战斗
       click(x, y)
       sleep(10000)
       break
-    case 6: // 行走状态
-      // 判断当前位置确定行走方向
-      judgeWalk()
+    case 6: // 确认状态
+      click(x, y)
+      sleep(10000)
+    case 7: // 城堡状态
+      click(x, y)
+      sleep(500)
+      switchTeam()
+      break
+    case 8: // 队伍状态
+      click(x, y)
+      sleep(500)
+      break
+    case 9: // 地图选择状态
+      click(x, y)
+      sleep(500)
+      break
+    case 10: // 行走状态
+      if (food < 20) {
+        click(x, y)
+      } else {
+        judgeMove()
+      }
+      sleep(400)
+      break
+    case 11: // 城堡状态
+      closeCastle()
       sleep(500)
       break
     default: // 未知状态
       // 停止脚本
-      stopFlag = true
+      exit()
       break
   }
 
-  if (stopFlag) {
-    exit()
-    return
-  } else {
-    nextStep()
+  nextStep()
+}
+
+// 判断当前状态
+function judgeState() {
+  var targetImg = null // 目标图片
+  var targetPath = '../autoImgs/dixiachengbao/target/' // 目标图片路径
+  var i = 1
+  var result = null
+
+  var nowView = captureScreen() // 当前截图`
+
+  while(i < 11 && !result) {
+    i++
+    targetImg = images.read(targetPath + i + '.jpg')
+    result = findImage(nowView, targetImg)
+  }
+
+  return {
+    state: i,
+    result: result
   }
 }
 
-function judgeWalk() {
-
-  if (food < 20) {
-    exit()
-    return
-  }
+// 判断移动
+function judgeMove() {
 
   if (direction === 'top') {
     if (nowLocation[1] > mapTop) {
       nowLocation[1] = nowLocation[1] - 1
-      click(500, 500)
+      click(500, 600)
       food = food - 4
     } else {
-      rightWalk()
+      xMove()
     }
   } else {
     if (nowLocation[1] < mapBottom) {
       nowLocation[1] = nowLocation[1] + 1
-      click(500, 1500)
+      click(500, 1600)
       food = food - 4
     } else {
-      rightWalk()
+      xMove()
     }
   }
 
 }
 
-function rightWalk() {
+// x轴移动
+function xMove() {
   rigtStep = rigtStep < 1 ? 3 : rigtStep
 
   if (rigtStep === 3) {
@@ -129,10 +147,52 @@ function rightWalk() {
   if (nowLocation[0] < mapRight - 1) {
     rigtStep --
     nowLocation[0] = nowLocation[0] + 1
-    click(800, 1000)
-    sleep(300)
+    click(800, 1100)
+    sleep(500)
     food = food - 4
   } else {
-    exit()
+    food = 10
   }
+}
+
+// 切换队伍
+function switchTeam() {
+  var targetPath = '../autoImgs/dixiachengbao/target/team1.jpg' // 目标图片路径
+  var targetImg = images.read(targetPath)
+  var nowView = captureScreen() // 当前截图
+
+  var result = findImage(nowView, targetImg)
+
+  if (result && result.x) {
+    console.log('切换1队')
+  } else {
+    targetPath = '../autoImgs/dixiachengbao/target/team2.jpg'
+    targetImg = images.read(targetPath)
+    result = findImage(nowView, targetImg)
+  }
+  if (result) {
+    click(result.x, result.y)
+  }
+  
+  resetData()
+  sleep(500)
+}
+
+// 关闭城堡
+function closeCastle() {
+  var targetImg = images.read('../autoImgs/dixiachengbao/target/close.jpg')
+  var nowView = captureScreen()
+  var result = findImage(nowView, targetImg)
+
+  if (result) {
+    click(result.x, result.y)
+  }
+}
+
+// 回城，重置地图行走数据
+function resetData() {
+  food = initialFood
+  direction = initialDirection
+  rigtStep = 0
+  nowLocation = [initialLocation[0], initialLocation[1]]
 }
