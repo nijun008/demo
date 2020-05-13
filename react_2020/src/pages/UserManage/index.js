a0a84d05e1ffd26382ca299fe1997e8c8b9c8bee
NiJun <87179471@qq.com>
2020-05-13 15:33:29 +0800

    add: react antd tabale

--- react_2020/src/pages/UserManage/index.js
+++ react_2020/src/pages/UserManage/index.js
@@ -1,10 +1,59 @@
 import React from 'react'
+import { Table, Tag, Button } from 'antd'
 
 class UserManage extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    const stateFormat = [
+      { label: '正常', tagColor: 'green' },
+      { label: '禁用', tagColor: 'gold' },
+      { label: '其它', tagColor: 'blue' }
+    ]
+
+    this.state = {
+      users: [
+        { id: '245515151', state: 0, nickName: '无', wechatName: '小二' },
+        { id: '342455151', state: 1, nickName: '德玛西亚皇子', wechatName: '四阿哥' },
+        { id: '124551516', state: 2, nickName: '瞎子', wechatName: '李青' }
+      ],
+      userCols: [
+        { title: '用户ID', dataIndex: 'id', key: 'id' },
+        { title: '昵称', dataIndex: 'nickName', key: 'nickName' },
+        { title: '微信昵称', dataIndex: 'wechatName', key: 'wechatName' },
+        { title: '状态', dataIndex: 'state', key: 'state', 
+          render: state => (
+            <Tag color={ stateFormat[state].tagColor }>{stateFormat[state].label}</Tag>
+          ) 
+        },
+        { 
+          title: '操作',
+          key: 'action',
+          render: (text, row, index) => (
+            <Button type="primary" size="small" ghost onClick={ () => this.tableDisableHandle(row, index) }>禁用</Button>
+          )
+        }
+      ]
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      let users = this.state.users.map(item => item)
+      users.push({ id: '512551516', state: 2, nickName: '一分三千喷', wechatName: '堡垒' })
+      this.setState({ users })
+    }, 2000)
+  }
+
+  tableDisableHandle = (row, index) => {
+    console.log(row, index)
+  }
+
   render() {
     return (
       <div>
-        用户管理
+        <Table dataSource={ this.state.users } columns={ this.state.userCols } rowKey="id" />
       </div>
     )
   }
